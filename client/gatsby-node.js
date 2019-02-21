c65761fdff15db9f4c040f661afe095cb71046aa
keidrun <kssk96@gmail.com>
2019-02-20 21:40:56 -0800

    Add templates and styling

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -3,5 +3,71 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path');
+const slash = require('slash');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions;
+  createRedirect({
+    fromPath: '/',
+    toPath: '/home',
+    redirectInBrowser: true,
+    isPermanent: true,
+  });
+
+  const result = await graphql(`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            link
+            status
+            template
+            title
+            content
+            slug
+          }
+        }
+      }
+      allWordpressPost {
+        edges {
+          node {
+            id
+            link
+            status
+            template
+            format
+            title
+            content
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw new Error(result.errors);
+  }
+
+  const { allWordpressPage, allWordpressPost } = result.data;
+
+  const pageTemplate = path.resolve(`./src/templates/page.js`);
+  allWordpressPage.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.slug}/`,
+      component: slash(pageTemplate),
+      context: edge.node,
+    });
+  });
+
+  const postTemplate = path.resolve(`./src/templates/post.js`);
+  allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: `/post/${edge.node.slug}/`,
+      component: slash(postTemplate),
+      context: edge.node,
+    });
+  });
+};
